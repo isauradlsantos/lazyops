ef43779759e857bdda9e80a7d1c2c31fb29bd9e9
Isaura Santos <isauradlsantos@gmail.com>
2025-07-01 11:23:02 +0100

    Add Vercel Analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
@@ -22,6 +23,7 @@ function App() {
       <ProofSection />
       <NextStepsSection />
       <Footer />
+      <Analytics />
     </div>
   );
 }
